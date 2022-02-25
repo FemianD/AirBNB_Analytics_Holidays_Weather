33e4d1d4ece82b3b70686abb3a9fc7fba2d45302
FemianD <98589496+FemianD@users.noreply.github.com>
2022-02-25 12:36:04 +0100

    Added print to file_url function
    When downloading it now says "Now downloading" and "Done!"

--- Code Airbnb.R
+++ Code Airbnb.R
@@ -4,18 +4,18 @@ library(tidyverse)
 
 # Read csv.gz from url
 Grabremote <- function(file_url) {
+  print("Now downloading.")
   con <- gzcon(url(file_url))
   txt <- readLines(con)
+  print("Done!")
   return(read.csv(textConnection(txt)))
 }
 
 #Load data
-amsterdam <- Grabremote("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-12-05/data/calendar.csv.gz")
-antwerp <- Grabremote ("http://data.insideairbnb.com/belgium/vlg/antwerp/2021-12-26/data/calendar.csv.gz")
-brussel <- Grabremote("http://data.insideairbnb.com/belgium/bru/brussels/2021-12-20/data/calendar.csv.gz")
-Madrid <- Grabremote("http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-12-08/data/calendar.csv.gz")
+amsterdam <- Grabremote("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-03-04/data/calendar.csv.gz")
+paris <- Grabremote ("http://data.insideairbnb.com/france/ile-de-france/paris/2021-03-04/data/calendar.csv.gz")
 
-combined_data <- bind_rows(list(amsterdam, antwerp), .id = "id") 
+combined_data <- bind_rows(list(amsterdam, paris), .id = "id") 
 combined_data <- as.data.frame(combined_data)
 
 data_datum_price <- combined_data %>%
